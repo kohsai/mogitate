45bcccf9d91ecf54a7e2e6283f3b0886b46b2226
kohsai <saishokoudai@gmail.com>
2025-04-27 17:19:15 +0900

    修正③　ProductSeasonモデルの修正

--- src/app/Models/ProductSeason.php
+++ src/app/Models/ProductSeason.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class ProductSeason extends Model
 {
     use HasFactory;
+
+    protected $table = 'product_season';
 }
+
+// ※
+// Laravelの規約では、モデル名が「複数形」で、テーブル名も「複数形」である前提（例：Product → products）。
+
+// でも、中間テーブルは特別で「単数_単数」(product_season)みたいな名前になりがち。
+
+// その場合、Laravelに「このモデルは product_season テーブルを使うんだよ！」と教えてあげるために、$table を明示してる。
